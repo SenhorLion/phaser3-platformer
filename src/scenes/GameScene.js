71e4989ab82e4bc48cc575bad61d2be4155eab80
Lionel Adams <lionel.adams@diconium.com>
2020-04-22 18:37:47 +0100

    feat: add player, player movement and stars

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,19 +1,24 @@
 import Phaser from "phaser";
 
+import ScoreLabel from "../ui/ScoreLabel";
+
 const GROUND_KEY = "ground";
 const DUDE_KEY = "dude";
+const STAR_KEY = "star";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super("game-scene");
 
     this.player = undefined;
+    this.cursors = undefined;
+    this.scoreLabel = undefined;
   }
 
   preload() {
     this.load.image("sky", "assets/sky.png");
     this.load.image(GROUND_KEY, "assets/platform.png");
-    this.load.image("star", "assets/star.png");
+    this.load.image(STAR_KEY, "assets/star.png");
     this.load.image("bomb", "assets/bomb.png");
 
     this.load.spritesheet(DUDE_KEY, "assets/dude.png", {
@@ -29,11 +34,13 @@ export default class GameScene extends Phaser.Scene {
     platforms.create(600, 400, GROUND_KEY);
     platforms.create(50, 250, GROUND_KEY);
     platforms.create(750, 220, GROUND_KEY);
+
+    return platforms;
   }
   createPlayer() {
-    this.player = this.physics.add.sprite(100, 450, DUDE_KEY);
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
+    const player = this.physics.add.sprite(100, 100, DUDE_KEY);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
     this.anims.create({
       key: "left",
@@ -54,11 +61,72 @@ export default class GameScene extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
+
+    return player;
+  }
+  createStars() {
+    const stars = this.physics.add.group({
+      key: STAR_KEY,
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+    });
+
+    stars.children.iterate((child) => {
+      child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.6));
+    });
+
+    return stars;
+  }
+  collectStar(player, star) {
+    star.disableBody(true, true);
+
+    // this.score += 10;
+    // scoreText.setText(`Score: ${this.score}`);
+    this.scoreLabel.add(10);
+  }
+  createScoreLabel(x, y, score) {
+    const style = { fontSize: "32px", fill: "#000" };
+    const label = new ScoreLabel(this, x, y, score, style);
+
+    this.add.existing(label);
+
+    return label;
   }
   create() {
     this.add.image(400, 300, "sky");
 
-    this.createPlatforms();
-    this.createPlayer();
+    const platforms = this.createPlatforms();
+    const stars = this.createStars();
+    this.scoreLabel = this.createScoreLabel(16, 16, 0);
+    this.player = this.createPlayer();
+
+    this.physics.add.collider(this.player, platforms);
+    this.physics.add.collider(stars, platforms);
+
+    this.physics.add.overlap(this.player, stars, this.collectStar, null, this);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+  handlePlayerMove() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
+  }
+  update() {
+    this.handlePlayerMove();
   }
 }
